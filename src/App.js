4bab01a0632492c96be09af18087343af733bed9
root <root@DESKTOP-VSC2SN6.localdomain>
2019-12-12 22:21:03 +0330

    ..

--- src/App.js
+++ src/App.js
@@ -30,7 +30,7 @@ const Skills = [
   { skill: 'Jira', level: '4' },
   { skill: 'Git', level: '4' },
 ]
-const SoftSkills = [{ skill: 'Problem-Solving', Level: '4' },
+const SoftSkills = [{ skill: 'Problem-solving', Level: '4' },
   { skill: 'Communication', Level: '5' },
   { skill: 'Teamwork', Level: '5' },
   { skill: 'Adaptability', Level: '4' },
@@ -39,15 +39,18 @@ const SoftSkills = [{ skill: 'Problem-Solving', Level: '4' },
   { skill: 'Time Management', Level: '5' },
   { skill: 'Leadership', Level: '4' },
 ]
-const Education = [{ since: '2018', Title: 'B.E. at Ghiaseddin Jamshid Kashani University', description: 'Ghazvin, Iran', }, { since: '2019', Title: `On the top 1% in the Master's Entrance Exam `, description: `On the top 1% of the entire participants in the Master's Entrance Exam (ranked 446 out of over 614000  participants)`, }, { since: 'Present', Title: 'MSc at Khaje Nasir Tousi University',Place:'Tehran, Iran' ,description: '1256th University of the World,15th of Iran', }]
-const AboutMe = [`Experience in Agile methodology of Scrum and Waterfall model.`,`Expertise in using Rally to manage Scrum/Agile sprints and user stories.`,`Ability to exercise independent judgment, project management skills and proficiency in planning.`,`Capability to manage multiple projects and/or tasks and adept new technologies with minimal learning curve.`,`Excellent problem solving skills with strong technical background and good interpersonal skills.`,`Quick learner and excellent team player, ability to meet deadlines and work under pressure.`,]
+const Education = [{ since: '2018', Title: 'B.E. at Ghiaseddin Jamshid Kashani University', description: 'Ghazvin, Iran', }, { since: '2019', Title: `On the top 1% in the Master's Entrance Exam `, description: `On the top 1% of the entire participants in the Master's Entrance Exam (ranked 519 out of over 614000  participants)`, }, { since: 'Present', Title: 'MSc at Khaje Nasir Tousi University', Place: 'Tehran, Iran', description: 'The 1256th University of the World,15th of Iran', }]
+const AboutMe = [`Experience in Agile methodology of Scrum and Waterfall model.`,`Expertise in using Rally to manage Scrum/Agile sprints and user stories.`,`Ability to exercise independent judgment, project management skills and proficiency in planning.`,`Capability to manage multiple projects and/or tasks and adept new technologies with a minimal learning curve.`,`Excellent problem-solving skills with a strong technical background and good interpersonal skills.`,`Quick learner and excellent team player, ability to meet deadlines and work under pressure.`,]
 const workingexperience = [{
   Since: '2019',
   Title: 'Rubika',
+  CompanyType:'LLC',
+  Description: `Rubika became Iran's most downloaded app in 2018 with an estimated 30 million downloads`,
+  // Rubika has been downloaded about 30 million times in the Iran',
   Position: 'Full-Stack Developer',
   Environment: `HTML5, CSS3, LESS, JavaScript, Reactjs, Restful API, Node.js, Mongo DB, AGILE, Material UI, Rest API's, Webpack, Jasmine, NPM, GIT, JIRA.`
   , detail: [
-    ' Created micro-services for decisions, mandates, meetings and documents using node.js, And deployed the docker containers.',
+    ' Created micro-services for decisions, mandates, meetings, and documents using node.js, And deployed the docker containers.',
     ' Worked with NPM commands and using package.json for managing dependencies and dev - dependencies of NodeJS applications.',
     ' Built a single page application with loading multiple views using route services and adding more user experience.',              
              ' Planned and implemented backup and recovery of the Mongo databases.',           
@@ -60,31 +63,34 @@ const workingexperience = [{
  
   {
     Since: '2018',
-    Title: 'Hamrahe Aval ( First Operator )',
+    Title: 'Mobile Telecommunication Company of Iran (Hamrahe Aval)',
+    CompanyType:'Corporation',
+    Description:'The first and largest mobile operator in Iran',
     Position: 'Full-Stack Javascript Developer',
     Environment: `Node.js, ReactJS, Redux, MongoDB, GIT, HTML5, CSS3, JSON, Bootstrap, WebPack, Babel.`
     , detail: [
       ` Working end to end, requirement gathering, designing, development, and implementation for the enhancements.`,
       ` Developed well responsive single page application using ReactJS, Redux, and Node.js in combination with HTML5, CSS3, and MongoDB.`,
       ` I Used the Bootstrap framework for building a responsive website for different devices, such as desktop, laptop, mobile, and iPad.`,
-      ` Developed front end user interface components using ReactJS, Redux, and JavaScript(ES6) and Integration of user - facing elements with server - side logic with Node.js, etc.`,
-      ` Developed unit tests for each new feature implemented using the Jasmine testing framework.`,
+      ` Developed front end user interface components using ReactJS, Redux, and JavaScript(ES6) and Integration of user-facing elements with server-side logic with Node.js, etc.`,
+      ` Advanced unit tests for each new feature implemented using the Jasmine testing framework.`,
       ` Work as part of the engineering team in a Scrum environment throughout the complete design and implementation process.`,
-      ` Designed REST API endpoints for communication with the client - side interface.`,
+      ` Designed REST API endpoints for communication with the client-side interface.`,
     ]
 
   },
   {
     Since: '2017',
     Title: 'Pardis Hooshmand Aseman',
+    CompanyType:'LLC',
     Position: 'Front-End Developer',
     Environment: ` React, Redux, Visual studio code, IntelliJ, JEST, HTML5, LESS, react, Web pack, NPM, Yarn, ESLint, GIT, Rest APIs.`
     , detail: [
-      ' Design and development of React Redux framework to create component - based scalable applications.',
-      ' Created Search form for a variety of search parameters with different combinations for data.',
-      ' Wrote, refactored and tested dozens of new components, including best practices for styling in a large code base.',
+      ' Design and development of React-Redux framework to create component-based scalable applications.',
+      ' Built Search form for a variety of search parameters with different combinations for data.',
+      ' Wrote, refactored, and tested dozens of new components, including best practices for styling in a large codebase.',
       ` Developed Rule Editor React component for creating rules with conditional statements and also for populating existing rules from API's.  `,
-      ` Created React Redux framework to generate dynamic forms, panels, and pages from users configuration files.`,
+      ` Established React-Redux framework to generate dynamic forms, panels, and pages from users configuration files.`,
     ]
 
   },
@@ -92,13 +98,14 @@ const workingexperience = [{
   {
     Since:'2016',
     Title: 'Shegeftangizan',
+    CompanyType:'LLC',
     Position: ' Front-End Developer',
     Environment: `Node.js,Mongoose, Reactjs, MongoDB, MS SQL Server, GIT, HTML5, CSS3, JSON, Bootstrap. `
     , detail: [
-` Developed, enhanced and corrected bugs in UI using JavaScript, JQuery, Reactjs, HTML5, and CSS3.`,
+` Developed, enhanced, and corrected bugs in UI using JavaScript, JQuery, Reactjs, HTML5, and CSS3.`,
 ` Designed and developed RESTful API for consuming in UI.`,
-` Integration of user - facing elements with server - side logic with Node.js etc.`,
-` Created and managed all of hosted or local repositories through GIT using Fork, Pull request and Push.`,
+` Integration of user-facing elements with server-side logic with Node.js etc.`,
+` Created and managed all of hosted or local repositories through GIT using Fork, Pull request, and Push.`,
 ` Peer code review and Unit testing, CIT(Continuous Integrated Testing) phase.`,
     ]
 
@@ -106,14 +113,16 @@ const workingexperience = [{
   {
     Since: '2014',
     Title: 'Ladybird',
+    CompanyType:'LLC',
     Position: ' Front-End Developer',
+
     Environment: `
     HTML5, CSS3, LESS, Material UI, JavaScript, Restful API, ES6, ReactJs,Node.js, Mongo DB, Agile, Bootstrap, REST API's, Webpack, Jasmine, NPM, GIT, JIRA, and Visual Studio Code.`
     , detail: [
-      ` Created front end user interface using ReactJs, HTML5, LESS to interact with microservices.Used MongoDB a NoSQL database for Data storage.Created API Gateway to communicate front end and microservices.`,
-      ` Responsible for Style, look and feel of the web page with LESS that extends CSS with dynamic behavior such as variable, mixing, operations and functions.`,
-      ` Implemented Restful API from the core using NodeJs and react Framework.`,
-      ` Designed dynamic client - side JavaScript codes to build web forms and simulate process for a web application, page navigation, and form validation.`,
+      ` Created front end user interface using ReactJs, HTML5, LESS to interact with microservices. Used MongoDB, a NoSQL database for Data storage.Created API Gateway to communicate front end and microservices.`,
+      ` Responsible for Style, look and feel of the web page with LESS that extends CSS with dynamic behavior such as variable, mixing, operations, and functions.`,
+      ` Implemented Restful API from the core using NodeJs and React Framework.`,
+      ` Designed dynamic client-side JavaScript codes to build web forms and simulate process for a web application, page navigation, and form validation.`,
       ` Created react reusable components and services to consume REST API's using Component-based architecture.`,
       ` Used GIT for source control and JIRA for tracking purposes.`,
       ` Also Worked within an AGILE team environment and participated in multiple scrum meetings to discuss planning and development needs for the current cycle.`,
@@ -139,14 +148,32 @@ const worksexperiences = workingexperience.map((workItem) =>
       </Grid>
             <Grid item xs={12}>
               <Typography variant="h4" style={{ fontWeight: '600' }}>
-                {workItem.Title}
+                {workItem.Position}
+              
                 </Typography>
       </Grid>
-      <Grid item>
-              <Typography variant="h5" style={{ fontWeight: '500' }}>
-                {workItem.Position}
+            <Grid item xs={12}>
+              <Typography variant="h6" style={{ fontWeight: '600' }}>
+                {workItem.Title}
+              
+
                 </Typography>
-     </Grid>
+      </Grid>
+            <Grid item xs={12}>
+              <Typography variant="h6" style={{ fontWeight: '500' }}>
+                {workItem.CompanyType}
+                </Typography>
+      </Grid>
+      <Grid item xs={12}>
+              <Typography variant="h6" >
+                {workItem.Description}
+                </Typography>
+            </Grid>
+            <Grid xs={12}>
+              <Typography variant="h6" style={{ fontWeight: '700' }}>
+              Key Qualification & Responsibilities
+              </Typography>
+            </Grid>
       <Grid item xs={11}>
         {workItem.detail.map((detailll) =>
           <Grid container spacing={3} style={{marginLeft:'4px'}}>
@@ -267,10 +294,10 @@ function ScrollableTabsButtonAuto() {
           style={{borderRadius:'10px'}}
         >
           <Tab label="Skills" {...a11yProps(0)} />
-          <Tab label="Working experience" {...a11yProps(1)} />
+          <Tab label="Work experience" {...a11yProps(1)} />
           <Tab label="Education" {...a11yProps(3)} />
           {/* <Tab label="About Me" {...a11yProps(2)} /> */}
-         <Tab label="Contact Me" {...a11yProps(4)} />
+         <Tab label="Contact Information" {...a11yProps(4)} />
           {/*
           <Tab label="Item Six" {...a11yProps(5)} />
           <Tab label="Item Seven" {...a11yProps(6)} /> */}
@@ -366,26 +393,62 @@ function ScrollableTabsButtonAuto() {
       </TabPanel> */}
     
       <TabPanel value={value} index={3}>
-        <Grid container>
-          <Grid item xs={12} md={4}>
+        <Grid container spacing={4}>
+          <Grid item xs={12} md={6}>
 
-        <a href="mailto:mohammadapi@live.com?Subject=Hello%20Mohammadamin" target="_top" style={{textDecoration:'none'}}>
+            <a href="mailto:mohammadapi@live.com?Subject=Hello%20Mohammad Amin" target="_top" style={{ textDecoration: 'none' }}>
               <Paper style={{ minHeight: '100px' }} textAlign="center">
-                <Grid container style={{ minHeight:'100px'}}
+                <Grid container style={{ minHeight: '100px' }}
                   direction="row" justify="center"
                   alignItems="center">
                   <Grid item xs={12} align="center">
-                <Typography variant="h6" textAlign="center">
-           Mohammadapi{at}Live.com
+                    <Typography variant="h6" textAlign="center">
+                      Mohammadapi{at}Live.com
             </Typography>
-</Grid>
-                  
+                  </Grid>
+
                 </Grid>
-            </Paper>
-          </a>
+              </Paper>
+            </a>
           </Grid>
+          <Grid item xs={12} md={6}>
+
+            <a href="tel:+989203046020" target="_top" style={{ textDecoration: 'none' }}>
+              <Paper style={{ minHeight: '100px' }} textAlign="center">
+                <Grid container style={{ minHeight: '100px' }}
+                  direction="row" justify="center"
+                  alignItems="center">
+                  <Grid item xs={12} align="center">
+                    <Typography variant="h6" textAlign="center">
+                  +989203046020
+            </Typography>
+                  </Grid>
+
+                </Grid>
+              </Paper>
+            </a>
+          </Grid>
+          <Grid item xs={12} md={12}>
+
+              <Paper style={{ minHeight: '100px' }} textAlign="center">
+                <Grid container style={{ minHeight: '100px' }}
+                  direction="row" justify="center"
+                  alignItems="center">
+                  <Grid item xs={12} align="center">
+                    <Typography variant="h6" textAlign="center">
+                No.15, Saeedi Sreet, Nelson Mandela bouldvard, Tehran, Iran
+            </Typography>
+                  </Grid>
+
+                </Grid>
+              </Paper>
+          </Grid>
+          {/* {EducationsItem} */}
         </Grid>
+
+
       </TabPanel>
+     
       <TabPanel value={value} index={2}>
         <Grid container spacing={4}>
           {EducationsItem}
@@ -435,11 +498,11 @@ function App() {
          
         }} >
           <Typography variant="h3" style={{marginTop:'10px',marginBottom:'10px'}}>
-           Mohammadamin
+           Mohammad Amin
              
             </Typography>
           <Typography variant="h5" >
-            I am a technically skilled full-stack web developer with over 6 years of experience.
+            I am a technically skilled full-stack developer with over six years of experience.
 I am a self-learner and highly motivated team player with the ability to work under pressure in complex environments.
     
          </Typography>
